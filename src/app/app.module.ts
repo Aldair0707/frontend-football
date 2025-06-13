29863f04414962b2928d46f4f1d3952b5636634e
Aldair0707 <aldairhernandez07124@gmail.com>
2025-06-12 21:47:29 -0600

    create branch users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NewUserComponent } from './new-user/new-user.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { TweetsComponent } from './tweets/tweets.component';
+import { HomeComponent } from './home/home.component';
+
+import {FormsModule } from "@angular/forms";
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NewUserComponent,
+    ForgotPasswordComponent,
+    ResetPasswordComponent,
+    LandingPageComponent,
+    TweetsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
